fc9aec00f6d524748f2101ec051aeba72f4ba26f
Ivan Kozyrin <ipkozyrin@gmail.com>
2026-07-28 18:54:14 -0300

    change

--- docs/landing-example/src/.vitepress/theme/index.ts
+++ docs/landing-example/src/.vitepress/theme/index.ts
@@ -1,5 +1,3 @@
-import type { EnhanceAppContext } from 'vitepress'
-import { resolveTranslationsByFilePath } from 'vitepress-theme-neptu-blog/utils'
 import LandingTheme from 'vitepress-theme-neptu-landing'
 import Layout from './Layout.vue'
 import './styles.css'
@@ -7,8 +5,4 @@ import './styles.css'
 export default {
   extends: LandingTheme,
   Layout,
-  enhanceApp(ctx: EnhanceAppContext) {
-    ctx.app.config.globalProperties.getLocales = () =>
-      resolveTranslationsByFilePath(ctx.router.route.path)
-  },
 }
