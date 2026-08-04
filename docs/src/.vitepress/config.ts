95e0f698a7c5b2f4d606123f2770444073b1b9ed
Ivan Kozyrin <ipkozyrin@gmail.com>
2026-08-03 22:02:01 -0300

    add icons

--- docs/src/.vitepress/config.ts
+++ docs/src/.vitepress/config.ts
@@ -32,8 +32,11 @@ export default async () => {
 
     head: [
       ['meta', { name: 'format-detection', content: 'telephone=no' }],
-      ['link', { rel: 'icon', sizes: '192x192', href: assetUrl('/img/android-chrome-192x192.png') }],
-      ['link', { rel: 'apple-touch-icon', sizes: '192x192', href: assetUrl('/img/android-chrome-192x192.png') }],
+      ['link', { rel: 'icon', type: 'image/svg+xml', href: assetUrl('/favicon.svg') }],
+      ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: assetUrl('/img/favicon-32x32.png') }],
+      ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: assetUrl('/img/favicon-16x16.png') }],
+      ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: assetUrl('/img/apple-touch-icon.png') }],
+      ['link', { rel: 'shortcut icon', href: assetUrl('/favicon.ico') }],
       ['link', { rel: 'manifest', href: assetUrl('/site.webmanifest') }],
     ],
 
